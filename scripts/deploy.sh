e854202656095e99bf9c282d85b50cbef895b6a6
msathe-tech <msathe@Madhavs-MacBook-Pro.local>
2019-05-29 02:29:25 -0400

    add amqp

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,15 +4,14 @@
 
 CF_API=`cf api | head -1 | cut -c 25-`
 
+# Deploy services
 if [[ $CF_API == *"api.run.pivotal.io"* ]]; then
-
     cf create-service cleardb spark fortunes-db
     cf create-service p-config-server trial fortunes-config-server -c '{"git": { "uri": "https://github.com/msathe-tech/fortune-teller", "searchPaths": "configuration" } }'
     cf create-service p-service-registry trial fortunes-service-registry
     cf create-service p-circuit-breaker-dashboard trial fortunes-circuit-breaker-dashboard
-
+    cf create-service cloudamqp lemur fortunes-cloud-bus
 else
-
     if [ ! -z "`cf m | grep "p\.config-server"`" ]; then
       export service_name="p.config-server"
       export config_json="{\"git\": { \"uri\": \"https://github.com/msathe-tech/fortune-teller\", \"searchPaths\": \"configuration\" } }"
@@ -28,8 +27,11 @@ else
     cf cs $service_name standard fortunes-config-server -c "$config_json"
     cf cs p-service-registry standard fortunes-service-registry
     cf cs p-circuit-breaker-dashboard standard fortunes-circuit-breaker-dashboard
-
+    cf create-service p.rabbitmq single-node-3.7 fortunes-cloud-bus
 fi
+
+# Prepare config file to set TRUST_CERTS value
+echo "cf_trust_certs: $CF_API" > vars.yml
 
 # Wait until services are ready
 while cf services | grep 'create in progress'
@@ -45,6 +47,5 @@ if cf services | grep 'create failed'; then
 fi
 echo "Service initialization - successful"
 
-echo "cf_trust_cert: $CF_API" > vars.yml
-
+# Push apps
 cf push -f manifest.yml --vars-file vars.yml
