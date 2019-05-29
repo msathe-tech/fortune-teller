230ba59bc0b60dcfecb0c16737ad7d1688ddf248
msathe-tech <msathe@Madhavs-MacBook-Pro.local>
2019-05-29 00:24:31 -0400

    optional amqp

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,8 @@ if [[ $CF_API == *"api.run.pivotal.io"* ]]; then
     cf create-service p-config-server trial fortunes-config-server -c '{"git": { "uri": "https://github.com/msathe-tech/fortune-teller", "searchPaths": "configuration" } }'
     cf create-service p-service-registry trial fortunes-service-registry
     cf create-service p-circuit-breaker-dashboard trial fortunes-circuit-breaker-dashboard
+
+    # cf create-service cloudamqp lemur fortunes-cloud-bus
 else
 
     if [ ! -z "`cf m | grep "p\.config-server"`" ]; then
@@ -25,6 +27,8 @@ else
     cf cs $service_name standard fortunes-config-server -c "$config_json"
     cf cs p-service-registry standard fortunes-service-registry
     cf cs p-circuit-breaker-dashboard standard fortunes-circuit-breaker-dashboard
+
+    # cf create-service p.rabbitmq single-node-3.7 fortunes-cloud-bus
 
 fi
 
